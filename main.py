3ac29ea005eb2d999567d3b12bb537b4eab9df2c
Logan Markley <114544357+loganmarkley@users.noreply.github.com>
2023-07-28 22:53:07 -0500

    Added Snake movement w/ key inputs
    Added snake movement and the ability to change direction of the snake with arrow keys

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Snake Game!
 # Author: Logan Markley
 # Last Updated: 7/28/2023
-# Version: 1.2
-# Latest Addition: added the Snake class that has a list of 2d vectors that contain its positions on the "grid"
+# Version: 1.3
+# Latest Addition: added snake movement, and the ability to change direction of the snake with arrow keys
 # Date Started: 7/28/2023
 # Desc: Using Clear Code's Youtube video "Learning pygame by creating Snake", the famous Snake game will be replicated
 
@@ -12,11 +12,17 @@ from pygame.math import Vector2
 class Snake:
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+        self.direction = Vector2(1,0)
 
     def draw_snake(self):
         for block in self.body:
             block_rect = pygame.Rect(block.x*cell_size, block.y*cell_size, cell_size, cell_size)
             pygame.draw.rect(screen, (183,191,122), block_rect)
+
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy[:]
 
 class Fruit:
     def __init__(self):
@@ -30,6 +36,7 @@ class Fruit:
         fruit_rect = pygame.Rect(self.pos.x*cell_size, self.pos.y*cell_size, cell_size, cell_size)      #Rect(x,y,w,h)
         pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
+
 pygame.init()
 cell_size = 40
 cell_number = 20
@@ -39,11 +46,25 @@ clock = pygame.time.Clock()
 fruit = Fruit()
 snake = Snake()
 
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)   #this event is triggered every 150 ms
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.direction = Vector2(0,-1)
+            if event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0,1)
+            if event.key == pygame.K_LEFT:
+                snake.direction = Vector2(-1,0)
+            if event.key == pygame.K_RIGHT:
+                snake.direction = Vector2(1,0)
 
     screen.fill((175,215,70))
     fruit.draw_fruit()
