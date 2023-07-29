099c330bf6a587383eefea0d006eb5dbcb7f9790
Logan Markley <114544357+loganmarkley@users.noreply.github.com>
2023-07-28 21:13:39 -0500

    no tutorial code

--- main.py
+++ main.py
@@ -11,17 +11,14 @@ import pygame, sys
 pygame.init()
 screen = pygame.display.set_mode((400,500))
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((100,200))
-test_surface.fill((0,0,255))
-test_rect = test_surface.get_rect(center = (200,250))
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
     screen.fill((175,215,70))
-    test_rect.right += 1
-    screen.blit(test_surface,test_rect) #block image transfer, center of the screen, but the top left is at that center
     pygame.display.update()
     clock.tick(60)      # game never runs faster than 60 times per second to make the game consistent
