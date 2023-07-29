369692c2f4778dcdcf1c885bc53a9a164974256c
Logan Markley <114544357+loganmarkley@users.noreply.github.com>
2023-07-28 21:29:56 -0500

    Added Fruit Class
    added the Fruit class with randomly generated position on the board

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 # Snake Game!
 # Author: Logan Markley
 # Last Updated: 7/28/2023
-# Version: 1.0
+# Version: 1.1
+# Latest Addition: added the Fruit class with randomly generated position on the board
 # Date Started: 7/28/2023
 # Desc: Using Clear Code's Youtube video "Learning pygame by creating Snake", the famous Snake game will be replicated
 
-import pygame, sys
+import pygame, sys, random
+from pygame.math import Vector2
 
+class Fruit:
+    def __init__(self):
+        # create a random x and y position
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)  # 2-d vector from pygame.math which stores the position of the fruit
+
+    def draw_fruit(self):
+        # create a rectangle and draw it
+        fruit_rect = pygame.Rect(self.pos.x*cell_size, self.pos.y*cell_size, cell_size, cell_size)      #Rect(x,y,w,h)
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
 pygame.init()
-screen = pygame.display.set_mode((400,500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))
 clock = pygame.time.Clock()
 
+fruit = Fruit()
 
 while True:
     for event in pygame.event.get():
@@ -20,5 +36,6 @@ while True:
             sys.exit()
 
     screen.fill((175,215,70))
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)      # game never runs faster than 60 times per second to make the game consistent
