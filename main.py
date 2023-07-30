4cc0466e01a66d70f77d9b75a2ae535c572fac70
Logan Markley <114544357+loganmarkley@users.noreply.github.com>
2023-07-30 14:25:28 -0500

    Added Snake's Body graphics! 7/30/23
    added 14 graphics to give the snake complete visuals

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Snake Game!
 # Author: Logan Markley
 # Last Updated: 7/30/2023
-# Version: 1.6
-# Latest Addition: added apple graphic
+# Version: 1.7
+# Latest Addition: added all of the snake's graphics!
 # Date Started: 7/28/2023
 # Desc: Using Clear Code's Youtube video "Learning pygame by creating Snake", the famous Snake game will be replicated
 
@@ -15,11 +15,72 @@ class Snake:
         self.direction = Vector2(1,0)
         self.new_block = False
 
-    def draw_snake(self):
-        for block in self.body:
-            block_rect = pygame.Rect(block.x*cell_size, block.y*cell_size, cell_size, cell_size)
-            pygame.draw.rect(screen, (183,191,122), block_rect)
+        self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('Graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('Graphics/head_left.png').convert_alpha()
 
+        self.tail_up = pygame.image.load('Graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('Graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('Graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('Graphics/tail_left.png').convert_alpha()
+
+        self.body_vertical = pygame.image.load('Graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('Graphics/body_horizontal.png').convert_alpha()
+
+        self.body_tr = pygame.image.load('Graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('Graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
+    def draw_snake(self):
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+        for index, block in enumerate(self.body):
+            x_pos = int(block.x*cell_size)
+            y_pos = int(block.y*cell_size)
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+
+            if index == 0:                                  #if the index is at the head
+                screen.blit(self.head, block_rect)
+            elif index == len(self.body) - 1:               #if the index is at the tail
+                screen.blit(self.tail, block_rect)
+            else:                                           #if the index is at a body part
+                previous_block = self.body[index+1] - block    #block's vector relation to the block 1 position ahead of it
+                next_block = self.body[index-1] - block   #block's vector relation to the block 1 position ahead of it
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect) #all 3 blocks are on the same column
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect) #all 3 blocks are on the same row
+                else:   #using the vector relations, we determine which image to display
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl, block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr, block_rect)
+                    else:
+                        screen.blit(self.body_br, block_rect)
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1,0):
+            self.head = self.head_left
+        elif head_relation == Vector2(-1,0):
+            self.head = self.head_right
+        elif head_relation == Vector2(0,1):
+            self.head = self.head_up
+        else:
+            self.head = self.head_down
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1, 0):
+            self.tail = self.tail_left
+        elif tail_relation == Vector2(-1, 0):
+            self.tail = self.tail_right
+        elif tail_relation == Vector2(0, 1):
+            self.tail = self.tail_up
+        else:
+            self.tail = self.tail_down
     def move_snake(self):
         if self.new_block == True:
             body_copy = self.body[:]
@@ -28,7 +89,6 @@ class Snake:
         body_copy.insert(0, body_copy[0] + self.direction)
         self.body = body_copy[:]
         self.new_block = False
-
     def add_block(self):
         self.new_block = True
 
@@ -85,7 +145,7 @@ clock = pygame.time.Clock()
 apple = pygame.image.load('Graphics/apple.png').convert_alpha()
 
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 140)   #this event is triggered every 150 ms
+pygame.time.set_timer(SCREEN_UPDATE, 150)   #this event is triggered every 150 ms
 
 main_game = Main()
 
